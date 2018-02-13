9a4dd8fe92d8bd2fdd0c3ccf03418b17218b7ab3
Chanwoo Chung <cwchung@mit.edu>
2018-02-12 20:13:59 -0500

    IP version updated for recent versions of Vivado

--- scripts/connectal-synth-zynq-mpsoc.tcl
+++ scripts/connectal-synth-zynq-mpsoc.tcl
@@ -3,7 +3,14 @@ source "$connectaldir/scripts/connectal-synth-ip.tcl"
 
 set prj_boardname $boardname
 
-connectal_synth_ip zynq_ultra_ps_e 2.0 zynq_ultra_ps_e_0 [list \
+set core_version "2.0"
+if {[version -short] >= "2017.3"} {
+    set core_version "3.1"
+}elseif {[version -short] >= "2017.1"} {
+    set core_version "3.0"
+}
+
+connectal_synth_ip zynq_ultra_ps_e $core_version zynq_ultra_ps_e_0 [list \
 							  CONFIG.PSU__FPGA_PL1_ENABLE {1} \
 							  CONFIG.PSU__USE__M_AXI_GP0 {1} \
 							  CONFIG.PSU__USE__IRQ0 {1} \
